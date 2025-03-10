09fe76a191d6b19de43a9707164da4b4cf2ff12c
Aleksander Parelo <aleksander@parelo.ee>
2025-03-10 21:59:58 +0200

    Add auth method and improve error handling

--- src/airobot_api_client/api.py
+++ src/airobot_api_client/api.py
@@ -15,10 +15,18 @@ class AirobotAPI:
         self.username = username
         self.password = password
     
+    def authenticate(self) -> bool:
+        settings_url = urljoin(self.thermostat_addr, "/api/thermostat/getSettings")
+        res = requests.get(settings_url, auth=(self.username, self.password))
+        
+        return res.ok
+
     def get_settings(self) -> ThermostatSettings:
         settings_url = urljoin(self.thermostat_addr, "/api/thermostat/getSettings")
         res = requests.get(settings_url, auth=(self.username, self.password))
-
+        
+        res.raise_for_status()
+        
         res_json = res.json()
         for flags in res_json["SETTING_FLAGS"]:
             for key, value in flags.items():
@@ -32,11 +40,15 @@ class AirobotAPI:
         inp = input.model_dump(exclude_defaults=True, exclude_unset=True)
         res = requests.post(settings_url, json=inp, auth=(self.username, self.password))
 
+        res.raise_for_status()
+
         return ThermostatSettingsUpdateResponse(**res.json())
     
     def get_status(self) -> ThermostatStatus:
         statuses_url = urljoin(self.thermostat_addr, "/api/thermostat/getStatuses")
         res = requests.get(statuses_url, auth=(self.username, self.password))
+
+        res.raise_for_status()
 
         res_json = res.json()
         for flags in res_json["STATUS_FLAGS"]:
--- src/airobot_api_client/async_api.py
+++ src/airobot_api_client/async_api.py
@@ -16,10 +16,14 @@ class AsyncAirobotAPI:
 
     async def __aexit__(self, *excinfo):
         await self.session.close()
+
+    async def authenticate(self) -> bool:
+        async with self.session.get("/api/thermostat/getSettings") as res:
+            return res.ok
     
     async def get_settings(self) -> ThermostatSettings:
-
         async with self.session.get("/api/thermostat/getSettings") as res:
+            res.raise_for_status()
             res_json = await res.json()
             for flags in res_json["SETTING_FLAGS"]:
                 for key, value in flags.items():
@@ -30,16 +34,16 @@ class AsyncAirobotAPI:
     async def set_settings(self, input: ThermostatSettingsUpdateInput) -> ThermostatSettingsUpdateResponse:
         inp = input.model_dump(exclude_defaults=True, exclude_unset=True)
         async with self.session.post("/api/thermostat/setSettings", json=inp) as res:
+            res.raise_for_status()
             res_json = await res.json()
             return ThermostatSettingsUpdateResponse(**res_json)
     
     async def get_status(self) -> ThermostatStatus:
         async with self.session.get("/api/thermostat/getStatuses") as res:
+            res.raise_for_status()
             res_json = await res.json()
             for flags in res_json["STATUS_FLAGS"]:
                 for key, value in flags.items():
                     res_json[key] = value
 
             return ThermostatStatus(**res_json)
-
-
